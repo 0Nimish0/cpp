9af19b124758217e445bfb3e71b7e88a90fdb241
0Nimish0 <nimishpawar1702@gmail.com>
2025-10-18 08:26:31 +0530

    data types started

---
+++ helloworld.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main(){
+
+    // int x = 5;
+    // int y = 6;
+    // int sum = x + y;
+
+    // std :: cout << x << "\n";
+    // std :: cout << y << "\n";
+    // std :: cout << sum << "\n";
+
+    //integer (whole number)
+
+    int age = 21;
+    int year = 2025;
+    int days = 7.5;
+
+    //double (number including decimal)
+
+    double price = 10.99;
+    double cgpa = 8.9;
+    double temp = 10;
+
+    // single character
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    // bpplean (true or False)
+
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    //string (Object that represnets a sequence of text)
+
+    std::string name = "Nimish";
+    std::string day = "Monday";
+    std::string food = "Pizza";
+    std::string address = "123 bla bla ";
+
+    std::cout << " Hello " << name << '\n';
+    std::cout << " You are " << age << " years old ";
+
+
+
+    return 0 ;
+}
