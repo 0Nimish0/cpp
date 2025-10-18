c4b1e45ff110b5fe107019a2d8f5968991822adb
0Nimish0 <nimishpawar1702@gmail.com>
2025-10-18 13:13:06 +0530

    moved

---
+++ 01_basics/arithmatic_opration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+
+    int students = 20;
+
+    // students++;
+    // std::cout << students;
+
+    // students--;
+    // std::cout << students << '\n';
+
+    // students*=2;
+    // std::cout << students << '\n';
+
+    students/=2;
+    std::cout << students << '\n';
+
+
+
+}
