45437d34fe8b61ed157ed5160a9acaab1990c1c1
0Nimish0 <nimishpawar1702@gmail.com>
2025-10-18 11:00:01 +0530

    namespace done

---
+++ 01_basics/namespace.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+namespace fist{
+    int x = 1;
+}
+
+namespace name{
+    int x = 3;
+} 
+
+
+
+int main(){
+
+    //Namespace = provides a solurion for preventing name conflicts 
+    //            in large projects. Each entity needs a unique name.
+    //            A namespace allows for indentically named entities 
+    //            as long as the namespace are differnt. 
+
+}
